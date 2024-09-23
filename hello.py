5e5ff1f61297fa805f2a22697bec2764d50505f2
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-09-23 10:05:40 +0200

    another commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+# name = input("Shyiramo izina ryawe sha kadooo:" ) #iyi ni input
+
+# age= int(input("shyiramo imyaka yawe sha:"))
+
+
+# print(f"we {name} ufite imyaka {age}")
+
+# if age < 18: 
+#     print(f"you are {age} and you are a minor ")
+# elif age >= 18 & age < 65 :
+#     print (f"your {age} makes you an adult ")
+    
+# else: print(f"you are {age} and you are very old")
+
+
+# for i in range(10) : print (i + 1)
+# count = 0
+# while count < 10 :
+#   print (count )  
+#   count += 1
+
+def square_a_number (num) :
+     
+  return num *num 
+
+print (square_a_number(5))
